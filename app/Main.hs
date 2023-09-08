5308c44d79f115818846f5a1adcbc1ff5319a7ab
parsonsmatt <parsonsmatt@gmail.com>
2023-09-08 16:20:24 -0600

    Basic exec functionality

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,56 @@
-module Main (main) where
+{-# language ApplicativeDo #-}
+
+module Main where
+
+import Data.Text (Text)
+import Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.List.NonEmpty as NEL
+import Options.Applicative
+import System.Process.Typed
+
+data Command
+    = Exec ExecArgs
+
+data ExecArgs = ExecArgs
+    { execArgsScript :: NonEmpty String
+    }
+
+optionsParser :: ParserInfo Command
+optionsParser =
+    info' parser' "Welcome to `hotel-california`"
+  where
+  -- thanks danidiaz for the blog post
+    info' :: Parser a -> String -> ParserInfo a
+    info' p desc = info
+        (helper <*> p)
+        (fullDesc <> progDesc desc)
+
+    parser' :: Parser Command
+    parser' = (subparser . foldMap command')
+        [ ("exec", "Execute the given command with tracing enabled", Exec <$> execParser)
+        ]
+
+    command' (cmdName,desc,parser) =
+        command cmdName (info' parser desc)
+
+    execParser :: Parser ExecArgs
+    execParser = do
+        execArgsScript1 <- argument str (metavar "SCRIPT")
+        execArgsScriptRest <- many $ argument str (metavar "SCRIPT")
+        pure ExecArgs
+            { execArgsScript = execArgsScript1 :| execArgsScriptRest
+            }
 
 main :: IO ()
-main = pure ()
+main = do
+    opts <- execParser optionsParser
+    case opts of
+        Exec ExecArgs {..} -> do
+            print ("args: ", execArgsScript)
+            exitCode <- runProcess $ shell $ unwords $ NEL.toList execArgsScript
+            print ("exit code: ", exitCode)
+
+
+
+
+
---
+++ src/HotelCalifornia/Exec.hs
@@ -0,0 +1,3 @@
+-- | This module defines the code for actually executing a command with tracing
+-- enabled.
+module HotelCalifornia.Exec where

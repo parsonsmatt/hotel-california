9df386d1e797086f3bcea575374a7a5bc1a56aaa
parsonsmatt <parsonsmatt@gmail.com>
2023-09-11 16:41:56 -0600

    refactored for structure

--- app/Main.hs
+++ app/Main.hs
@@ -1,37 +1,8 @@
-{-# language ApplicativeDo #-}
-
 module Main where
 
-import Data.ByteString.Char8 qualified as BS8
-import OpenTelemetry.Vendor.Honeycomb qualified as Honeycomb
-import Data.Maybe (fromMaybe)
-import Data.Text (Text)
-import Data.Text qualified as Text
-import GHC.Stack
-import Data.List.NonEmpty (NonEmpty(..))
-import qualified Data.List.NonEmpty as NEL
+import HotelCalifornia.Exec
+import HotelCalifornia.Tracing (withGlobalTracing)
 import Options.Applicative
-import System.Process.Typed
-import UnliftIO
-import OpenTelemetry.Context as Context hiding (lookup)
-import OpenTelemetry.Context qualified as Context
-import OpenTelemetry.Context.ThreadLocal (adjustContext, attachContext, getContext)
-import OpenTelemetry.Trace hiding
-  ( SpanKind (..),
-    SpanStatus (..),
-    addAttribute,
-    addAttributes,
-    createSpan,
-    inSpan,
-    inSpan',
-    inSpan'',
-  )
-import OpenTelemetry.Trace qualified as Trace
-import OpenTelemetry.Trace.Sampler
-import OpenTelemetry.Utils.Exceptions qualified as TraceM
-import OpenTelemetry.Vendor.Honeycomb qualified as Honeycomb
-import Control.Monad
-import Data.Time
 
 data Command = Command
     { commandGlobalOptions :: GlobalOptions
@@ -42,11 +13,6 @@ data GlobalOptions = GlobalOptions
 
 data SubCommand
     = Exec ExecArgs
-
-data ExecArgs = ExecArgs
-    { execArgsScript :: NonEmpty String
-    , execArgsSpanName :: Maybe Text
-    }
 
 optionsParser :: ParserInfo Command
 optionsParser =
@@ -70,81 +36,16 @@ optionsParser =
     subCommandParser :: Parser SubCommand
     subCommandParser =
         subparser $ foldMap command'
-                    [ ("exec", "Execute the given command with tracing enabled", Exec <$> execParser)
+                    [ ("exec", "Execute the given command with tracing enabled", Exec <$> parseExecArgs)
                     ]
 
     command' (cmdName,desc,parser) =
         command cmdName (info' parser desc)
-
-    execParser :: Parser ExecArgs
-    execParser = do
-        execArgsSpanName <- optional do
-            option str $ mconcat
-                [ metavar "SPAN_NAME"
-                , long "span-name"
-                , short 's'
-                ]
-        execArgsScript1 <- argument str (metavar "SCRIPT")
-        execArgsScriptRest <- many $ argument str (metavar "SCRIPT...")
-        pure ExecArgs
-            { execArgsScript = execArgsScript1 :| execArgsScriptRest
-            , ..
-            }
 
 main :: IO ()
 main = do
     withGlobalTracing do
         Command {..} <- execParser optionsParser
         case commandSubCommand of
-            Exec ExecArgs {..} -> do
-                let script =
-                        unwords $ NEL.toList execArgsScript
-                    spanName =
-                        fromMaybe (Text.pack script) execArgsSpanName
-
-                print ("args: ", execArgsScript)
-                tp <- globalTracer
-                exitCode <-
-                    inSpanIO spanName defaultSpanArguments do
-                        runProcess $ shell $ unwords $ NEL.toList execArgsScript
-                print ("exit code: ", exitCode)
-
-globalTracer :: MonadIO m => m Tracer
-globalTracer = getGlobalTracerProvider >>= \tp -> getTracer tp "hotel-california" tracerOptions
-
-inSpanIO :: (MonadUnliftIO m, HasCallStack) => Text -> SpanArguments -> m a -> m a
-inSpanIO t a m = globalTracer >>= \tr -> Trace.inSpan tr t a m
-
--- | Initialize the global tracing provider for the application and run an action
---   (that action is generally the entry point of the application), cleaning
---   up the provider afterwards.
---
---   This also sets up an empty context (creating a new trace ID).
-withGlobalTracing :: MonadUnliftIO m => m a -> m a
-withGlobalTracing act = do
-    void $ attachContext Context.empty
-    bracket initializeTracing shutdownTracerProvider $ \_ -> do
-        -- note: this is not in a span since we don't have a root span yet so it
-        -- would not wind up in the trace in a helpful way anyway
-        void $
-          Honeycomb.getOrInitializeHoneycombTargetInContext initializationTimeout
-            `catch` \(e :: SomeException) -> do
-              -- we are too early in initialization to be able to use a normal logger,
-              -- but this needs to get out somehow.
-              --
-              -- honeycomb links are not load-bearing, so we let them just not come
-              -- up if the API fails.
-              liftIO . BS8.hPutStrLn stderr $ "error setting up Honeycomb trace links: " <> (BS8.pack $ displayException e)
-              pure Nothing
-
-        act
-  where
-    initializationTimeout = secondsToNominalDiffTime 3
-
-initializeTracing :: MonadUnliftIO m => m TracerProvider
-initializeTracing = do
-  (processors, tracerOptions') <- liftIO getTracerProviderInitializationOptions
-  provider <- createTracerProvider processors tracerOptions'
-  setGlobalTracerProvider provider
-  pure provider
-
+            Exec execArgs ->
+                runExecArgs execArgs
--- src/HotelCalifornia/Exec.hs
+++ src/HotelCalifornia/Exec.hs
@@ -1,3 +1,47 @@
 -- | This module defines the code for actually executing a command with tracing
 -- enabled.
 module HotelCalifornia.Exec where
+
+import Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.List.NonEmpty as NEL
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
+import qualified Data.Text as Text
+import HotelCalifornia.Tracing
+import Options.Applicative
+import System.Exit
+import System.Process.Typed
+
+data ExecArgs = ExecArgs
+    { execArgsScript :: NonEmpty String
+    , execArgsSpanName :: Maybe Text
+    }
+
+parseExecArgs :: Parser ExecArgs
+parseExecArgs = do
+    execArgsSpanName <- optional do
+        option str $ mconcat
+            [ metavar "SPAN_NAME"
+            , long "span-name"
+            , short 's'
+            ]
+    execArgsScript1 <- argument str (metavar "SCRIPT")
+    execArgsScriptRest <- many $ argument str (metavar "SCRIPT...")
+    pure ExecArgs
+        { execArgsScript = execArgsScript1 :| execArgsScriptRest
+        , ..
+        }
+
+runExecArgs :: ExecArgs -> IO ()
+runExecArgs ExecArgs {..} = do
+    let script =
+            unwords $ NEL.toList execArgsScript
+        spanName =
+            fromMaybe (Text.pack script) execArgsSpanName
+
+    -- print ("args: ", execArgsScript)
+    exitCode <-
+        inSpan spanName do
+            runProcess $ shell $ unwords $ NEL.toList execArgsScript
+    -- print ("exit code: ", exitCode)
+    exitWith exitCode

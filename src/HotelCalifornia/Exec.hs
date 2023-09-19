fcd3e90f00f12bfd7b42e49981e012edadb851e9
parsonsmatt <parsonsmatt@gmail.com>
2023-09-19 13:52:05 -0600

    Set Sigint behavior

--- src/HotelCalifornia/Exec.hs
+++ src/HotelCalifornia/Exec.hs
@@ -2,6 +2,8 @@
 -- enabled.
 module HotelCalifornia.Exec where
 
+import qualified Control.Exception as Exception
+import qualified Data.Char as Char
 import Data.List.NonEmpty (NonEmpty(..))
 import qualified Data.List.NonEmpty as NEL
 import Data.Maybe (fromMaybe)
@@ -9,10 +11,11 @@ import Data.Text (Text)
 import qualified Data.Text as Text
 import HotelCalifornia.Tracing
 import HotelCalifornia.Tracing.TraceParent
-import System.Environment (getEnvironment)
-import qualified System.Posix.Escape.Unicode as Escape
+import qualified OpenTelemetry.Trace.Core as Otel
 import Options.Applicative hiding (command)
+import System.Environment (getEnvironment)
 import System.Exit
+import qualified System.Posix.Escape.Unicode as Escape
 import System.Process.Typed
 
 data Subprocess = Proc (NonEmpty String) | Shell String
@@ -28,7 +31,22 @@ commandToProcessConfig (Shell line) = shell line
 data ExecArgs = ExecArgs
     { execArgsSubprocess :: Subprocess
     , execArgsSpanName :: Maybe Text
+    , execArgsSigintStatus :: SpanStatus'
     }
+
+-- | A variant of 'SpanStatus' that does not include a 'Text' for error.
+data SpanStatus'
+    = SpanUnset
+    | SpanOk
+    | SpanError
+
+parseSpanStatus' :: ReadM SpanStatus'
+parseSpanStatus' = eitherReader \s ->
+    case map Char.toLower s of
+        "unset" -> Right SpanUnset
+        "ok" -> Right SpanOk
+        "error" -> Right SpanError
+        _ -> Left $ mconcat ["Expected one of `unset`, `ok`, or `error` for SPAN_STATUS. Got: ", s]
 
 parseProc :: Parser (NonEmpty String)
 parseProc = do
@@ -56,6 +74,14 @@ parseExecArgs = do
             , short 's'
             , help "The name of the span that the program reports. By default, this is the script you pass in."
             ]
+    execArgsSigintStatus <-
+        option parseSpanStatus' $ mconcat
+            [ metavar "SPAN_STATUS"
+            , long "set-sigint-status"
+            , short 'i'
+            , help "The status reported when the process is killed with SIGINT."
+            , value SpanUnset
+            ]
     execArgsSubprocess <- parseSubprocess
     pure ExecArgs{..}
 
@@ -71,9 +97,26 @@ runExecArgs ExecArgs {..} = do
 
         let processConfig = commandToProcessConfig execArgsSubprocess
 
-        exitCode <- runProcess $ setEnv fullEnv processConfig
-        case exitCode of
-            ExitSuccess ->
+        let handleSigInt =
+                \case
+                    Exception.UserInterrupt ->
+                        case execArgsSigintStatus of
+                            SpanUnset ->
+                                pure Nothing
+                            SpanOk -> do
+                                Otel.setStatus span_ Otel.Ok
+                                pure Nothing
+                            SpanError -> do
+                                Exception.throwIO Exception.UserInterrupt
+                    other ->
+                        Exception.throwIO other
+
+        mexitCode <- Exception.handle handleSigInt $ fmap Just $ runProcess $ setEnv fullEnv processConfig
+
+        case mexitCode of
+            Just ExitSuccess ->
                 pure ()
-            _ ->
+            Just exitCode ->
                 exitWith exitCode
+            Nothing ->
+                pure ()
